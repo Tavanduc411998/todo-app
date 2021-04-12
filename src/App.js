4e180fceb5042b067ee33b46774e2a4148f9a70c
Tavanduc411998 <tavanduc2018@gmail.com>
2021-04-12 09:14:08 +0700

    Todo App
    Todo App using localstorage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import React from "react";
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div className="todo"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => completeTodo(index)}>Complete/Uncomplete</button>
+        <button onClick={() => removeTodo(index)}>Remove</button>
+      </div>
+    </div>
+  )
+};
+
+function TodoForm({ addTodo }) {
+  const [newTodo, setNewTodo] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!newTodo) return;
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={newTodo}
+        onChange={e => setNewTodo(e.target.value)}
+      />
+    </form>
+  );
+}
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const saveData = (newTodos) => {
+    localStorage.setItem("todos", JSON.stringify(newTodos));
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("todos")) {
+      setTodos(JSON.parse(localStorage.getItem("todos")));
+    }
+  }, []);
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+    saveData(newTodos);
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    if (newTodos[index].isCompleted === true)
+      newTodos[index].isCompleted = false;
+    else newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+    saveData(newTodos);
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+    saveData(newTodos);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="todo-list">
+        <TodoForm addTodo={addTodo} />
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            todo={todo}
+            index={index}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
